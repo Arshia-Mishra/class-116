cf98edf8abd903e3f62ba908e53371304fe19348
Arshia-Mishra <80540337+Arshia-Mishra@users.noreply.github.com>
2022-10-28 15:20:43 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+noseX=0
+noseY=0
+
 function preload() {
-
+clown_nose = loadImage('https://i.postimg.cc/BvtkqFGd/clown-nose-1.png');
 }
-
 
 function setup() {
     canvas = createCanvas(300,300);
@@ -19,9 +21,9 @@ function modelLoaded(){
 }
 
 function draw(){
-image(video,0);
+image(video,0,0,300,300);
+image(clown_nose, noseX-15, noseY-15, 30,30);
 }
-
 
 function take_snapshot(){
     save('myFilterImage.png');
@@ -32,10 +34,9 @@ function gotPoses(results)
     if(results.length>0)
     {
         console.log(results);
-        console.log("lip x = " + results[0].pose.nose.x);
-        console.log("lip y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + results[0].pose.nose.X);
+        console.log("nose y = " + results[0].pose.nose.Y);
     }
 }
-
-
-
